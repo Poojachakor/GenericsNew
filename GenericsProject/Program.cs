c98cd62f030497ca04cffc4b225c12db2b06d15d
Poojachakor <chakorpooja07@gmail.com>
2023-06-17 00:01:36 +0530

    UC2 file

--- GenericsProject/Program.cs
+++ GenericsProject/Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Exit");
+                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Find Float Max \n3.Exit");
                 int options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -16,12 +16,15 @@
                         int result = FindMaximum.FindMax(23, 25, 27);
                         Console.WriteLine("Max Integer is: " + result);
                         break;
+                    case 2:
+                        float result1 = FindMaximum.FindMaxFloat(22.4f, 23.4f, 21.5F);
+                        Console.WriteLine("Max Float is: " + result1);
+                        break;
                     default:
                         flag = false;
                         break;
                 }
             }
         }
-
     }
 }
